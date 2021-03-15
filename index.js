6385c738a35cb69416303e1967cfcabffc615465
Howlan <rickvk183@gmail.com>
2021-03-15 15:01:23 +0530

    myFirstDatabase  -> oneshotAi

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const CollgModel = require('./models/Colleges')
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-mongoose.connect('mongodb+srv://rick:Rick@1999@oneshot-ai-backend.m3iuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://rick:Rick@1999@oneshot-ai-backend.m3iuo.mongodb.net/oneshotAi?retryWrites=true&w=majority',
                 {useNewUrlParser : true}
 );
 
