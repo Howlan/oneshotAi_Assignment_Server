e31752ac8844556d026ced82a4b7b9c538e51cde
Howlan <rickvk183@gmail.com>
2021-03-15 12:52:37 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// db link
+// mongodb://localhost:27017/<db_name>?readPreference=primary&appname=MongoDB%20Compass&ssl=false
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+const CollgModel = require('./models/Colleges')
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+mongoose.connect('mongodb://localhost:27017/oneshotAi?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+                {useNewUrlParser : true}
+);
+
+app.post('/addCollege', async (req, res) => {
+  
+    const name = req.body.name;
+    const id = req.body.id;
+    const city = req.body.city;
+    const noOfStudents = req.body.noOfStudents;
+    const state = req.body.state;
+    const country = req.body.country;
+    const courses = req.body.course;
+    const yearFounded = req.body.yearFounding;
+
+    const College = new CollgModel({
+        id : id,
+        name : name,
+        yearFounded : yearFounded,
+        city : city,
+        state : state,
+        country : country,
+        numberOfStudents : noOfStudents,
+        courses : courses });
+    await College.save()
+    res.send('Inserted Data');
+});
+
+
+app.get("/read", async (req, res) => {
+    CollgModel.find({}, (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+
+app.listen(process.env.PORT | 3001, () => {
+    console.log("Connected!")
+})
---
+++ models/Colleges.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const oneshotAi = new mongoose.Schema({
+    id : {
+        type: Number,
+        required : true
+    },
+    name :{
+        type : String,
+        required : true
+    },
+    yearFounded : {
+        type: Number,
+        required : true
+    },
+    city :{
+        type : String,
+        required : true
+    },
+    state :{
+        type : String,
+        required : true
+    },
+    country :{
+        type : String,
+        required : true
+    },
+    numberOfStudents :{
+        type : Number,
+        required : true
+    },
+    courses :{
+        type : String,
+        required : true
+    }
+});
+
+const CollgModel = mongoose.model('Colleges', oneshotAi);
+
+module.exports = CollgModel ;
